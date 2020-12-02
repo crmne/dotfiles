a91f099802c110fc81944acc66457c6b516bbb7f
Carmine Paolino <carmine@paolino.me>
2020-12-02 16:38:46 +0100

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+# Set greeting
+set fish_greeting
+
+# Add a few directories to the PATH
+set -x PATH $PATH ~/.local/bin
+
+# Editor: Visual Studio Code
+set -x EDITOR "code -w"
+set -x VISUAL $EDITOR
