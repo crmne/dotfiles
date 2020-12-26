527010a9f8f8c4ea30fd46181268df3f29b5ce61
Carmine Paolino <carmine@paolino.me>
2020-12-26 15:06:13 +0100

    conditional shell configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,25 @@
 set fish_greeting
 
 # Add a few directories to the PATH
-set -x PATH $PATH ~/.local/bin
+if [ -d ~/.local/bin ]
+    set -x PATH $PATH ~/.local/bin
+end
+## Homebrew M1
+if [ -d /opt/homebrew/bin ]
+    set -gx PATH /opt/homebrew/bin /opt/homebrew/opt $PATH
+end
 
-# Editor: Visual Studio Code
-set -x EDITOR "code -w"
+# Editor
+if type -q code
+    set -x EDITOR "code -w"
+else if type -q code-exploration
+    alias code code-exploration
+    set -x EDITOR "code-exploration -w"
+else if type -q vim
+    set -x EDITOR "vim"
+end
 set -x VISUAL $EDITOR
 
 # Set language
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
-
-# Homebrew M1
-set -gx PATH /opt/homebrew/bin /opt/homebrew/opt $PATH
