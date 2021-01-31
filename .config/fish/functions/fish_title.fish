d4a72158f75275c7683745f224262b2a792b3f9f
Carmine Paolino <carmine@paolino.me>
2021-01-31 16:40:07 +0100

    no terminal title

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,7 +1,7 @@
-# Defined in /var/folders/2k/9693sr6n44705kn2zq4zrthw0000gn/T//fish.ssNReu/fish_title.fish @ line 2
+# Defined in /Users/carmine/.config/fish/functions/fish_title.fish @ line 2
 function fish_title
     # emacs is basically the only term that can't handle it.
-    if not set -q INSIDE_EMACS
-        echo (status current-command) (prompt_pwd)
-    end
+    # if not set -q INSIDE_EMACS
+    #     echo (status current-command) (prompt_pwd)
+    # end
 end
