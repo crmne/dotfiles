626d1d8b295713a3de8a10289dffb7c8f357ad0e
Carmine Paolino <carmine@paolino.me>
2024-08-04 11:33:42 +0200

    linux changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,14 +28,14 @@ end
 # Editor
 if type -q nvim
     alias vim=nvim
-    set -x EDITOR "nvim"
+    set -x EDITOR nvim
 else if type -q code
     set -x EDITOR "code -w"
 else if type -q code-exploration
     alias code code-exploration
     set -x EDITOR "code-exploration -w"
 else if type -q vim
-    set -x EDITOR "vim"
+    set -x EDITOR vim
 end
 set -x VISUAL $EDITOR
 
@@ -44,12 +44,12 @@ set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f ~/google-cloud-sdk/path.fish.inc ]; . ~/google-cloud-sdk/path.fish.inc; end
+if [ -f ~/google-cloud-sdk/path.fish.inc ]
+    . ~/google-cloud-sdk/path.fish.inc
+end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
-# rbenv
-status --is-interactive; and rbenv init - fish | source
+mise activate fish | source
 
-# pyenv
-pyenv init - | source
+set -x IBUS_ENABLE_SYNC_MODE 2
