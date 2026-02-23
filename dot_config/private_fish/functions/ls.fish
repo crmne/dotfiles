6c30da4f72b651ba7fb4610540b1f41006181e89
Carmine Paolino <carmine@paolino.me>
2026-02-23 11:07:29 +0100

    also display group in ls

--- dot_config/private_fish/functions/ls.fish
+++ dot_config/private_fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='eza -lh --group-directories-first --icons=auto' --description 'alias ls=eza -lh --group-directories-first --icons=auto'
-    eza -lh --group-directories-first --icons=auto $argv
+function ls --wraps='eza -lhg --group-directories-first --icons=auto' --description 'alias ls=eza -lhg --group-directories-first --icons=auto'
+    eza -lhg --group-directories-first --icons=auto $argv
 end
